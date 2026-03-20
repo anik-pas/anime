a2f317a0f7dfe6fe8f7371dc19dc045492e0940a
anik-pas <anik-pas@ya.ru>
2026-03-20 12:27:48 +0300

    slider

--- js/bg-elements.js
+++ js/bg-elements.js
@@ -1,9 +1,13 @@
-const elements = document.querySelectorAll(".set-bg");
+const bgElements = () => {
+  const elements = document.querySelectorAll(".set-bg");
 
-for (let i = 0; i < elements.length; i++) {
-  const src = elements[i].dataset.setbg;
+  for (let i = 0; i < elements.length; i++) {
+    const src = elements[i].dataset.setbg;
 
-  // elements[i].style.backgroundImage = "url(" + src + ")"; эту строчку можно упростить см. ниже
-  elements[i].style.backgroundImage = `url(${src})`;
-  // console.dir(elements[i]);
-}
+    // elements[i].style.backgroundImage = "url(" + src + ")"; эту строчку можно упростить см. ниже
+    elements[i].style.backgroundImage = `url(${src})`;
+    // console.dir(elements[i]);
+  }
+};
+
+bgElements();
