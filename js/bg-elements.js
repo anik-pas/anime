af06540e6027a28e25e75ba69adfe67882abf643
anik-pas <anik-pas@ya.ru>
2026-03-20 09:10:23 +0300

    1 урок

---
+++ js/bg-elements.js
@@ -0,0 +1,9 @@
+const elements = document.querySelectorAll(".set-bg");
+
+for (let i = 0; i < elements.length; i++) {
+  const src = elements[i].dataset.setbg;
+
+  // elements[i].style.backgroundImage = "url(" + src + ")"; эту строчку можно упростить см. ниже
+  elements[i].style.backgroundImage = `url(${src})`;
+  // console.dir(elements[i]);
+}
