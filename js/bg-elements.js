a8881cd64102189216408b914487224afd1ece2f
anik-pas <anik-pas@ya.ru>
2026-03-20 19:42:56 +0300

    получали данные с сервера

--- js/bg-elements.js
+++ js/bg-elements.js
@@ -1,13 +1,9 @@
 const bgElements = () => {
   const elements = document.querySelectorAll(".set-bg");
 
-  for (let i = 0; i < elements.length; i++) {
-    const src = elements[i].dataset.setbg;
-
-    // elements[i].style.backgroundImage = "url(" + src + ")"; эту строчку можно упростить см. ниже
-    elements[i].style.backgroundImage = `url(${src})`;
-    // console.dir(elements[i]);
-  }
+  elements.forEach((elem) => {
+    elem.style.backgroundImage = `url(${elem.dataset.setbg})`;
+  });
 };
 
 bgElements();
